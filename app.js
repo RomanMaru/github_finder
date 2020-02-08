ed4e2cef9323d8ea1d8d9b37dcf1e02f37e420e4
Roman Maru <romanmaru10@gmail.com>
2020-02-07 17:39:31 -0800

    Add Alert whet user is not found

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const github = new Github
-
+const ui = new UI
 const searchUser = document.getElementById('searchUser')
 
 searchUser.addEventListener('keyup', event => {
@@ -9,12 +9,12 @@ searchUser.addEventListener('keyup', event => {
     github.getUser(userText)
     .then(data => {
       if (data.profile.message === 'Not Found') {
-        
+        ui.showAlert('User not found', 'alert alert-danger')
       } else {
-        
+        ui.showProfile(data.profile)
       }
     })
   } else {
-    
+    ui.clearProfile()
   }
 })
